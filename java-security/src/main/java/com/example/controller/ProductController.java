76cc7d3c4fd5047250e4a1961ee5276395bd5fba
kakerusaman <kakerusaman@gmail.com>
2026-06-12 00:36:23 +0900

    RestControllerで一覧画面を取得する処理を作成

--- java-security/src/main/java/com/example/controller/ProductController.java
+++ java-security/src/main/java/com/example/controller/ProductController.java
@@ -1,18 +1,25 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
 public class ProductController {
     
-    public String hoge(){
+    /*
+    商品一覧画面の表示
+    ゆくゆくは商品をジャンルごとで表示するなどのSort機能を作成することを考えたい
+    */
+    @GetMapping("/showProductList")
+    // 絞り込みのジャンルを取得したいデフォルトはデフォルト一覧画面
+    public String showProductList(@RequestParam(defaultValue = "findAll") String list){
 
-        try {
+        // DB内から全権取得する処理
             
-        } catch(Exception e) {
-            
-        } finally {
+        // それをフロント側が見やすいように整形する処理
 
-        }
-
-
-        return "hgoe";
+        return "respons";
     }
 }
